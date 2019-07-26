454aab67c92c7386626bdb3ab5b5507ec98a54c9
Patrick Ng <PJLN07@gmail.com>
2019-07-26 00:39:35 -0400

    ADD: functions to test calls to news API

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,17 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from newsapi import NewsApiClient
 app = Flask(__name__)
+CORS(app)
+newsapi = NewsApiClient(api_key=NEWS_API)
+
 
 @app.route('/')
-def hello_world():
-  return 'Hello World!'
+def fetch_articles():
+    top_headlines = newsapi.get_top_headlines(language='en')
+    return top_headlines
+
+@app.route('/<string:type>')
+def fetch_articles_type(type):
+    articles_by_type = newsapi.get_top_headlines(category=type, language='en', country='us')
+    return articles_by_type
