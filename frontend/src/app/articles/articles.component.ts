0e8ab24c1554bfad44914877c758c5824650fba5
Patrick Ng <PJLN07@gmail.com>
2019-07-27 22:49:34 -0400

    EDIT: component subscribes to changes in url

--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -1,23 +1,36 @@
 import { ArticlesService } from './articles.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
-  providers: [ArticlesService]
+  providers: [ArticlesService],
 })
 export class ArticlesComponent implements OnInit {
   headlineArticles: Object[];
   articles: Object[];
-  constructor(private articlesService: ArticlesService) { }
+  typeParam: String;
+  constructor(
+    private articlesService: ArticlesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.articlesService.fetchAllArticles()
-      .subscribe(res => {
-        this.headlineArticles = res.articles.slice(0,3);
-        this.articles = res.articles.slice(3);
-      })
+    this.route.queryParams.subscribe(params => {
+      this.typeParam = params.type || history.state.type;
+      if (this.typeParam) {
+        this.articlesService.fetchArticleType(this.typeParam).subscribe(res => {
+          this.headlineArticles = res.articles.slice(0, 3);
+          this.articles = res.articles.slice(3);
+        });
+      } else {
+        this.articlesService.fetchAllArticles().subscribe(res => {
+          this.headlineArticles = res.articles.slice(0, 3);
+          this.articles = res.articles.slice(3);
+        });
+      }
+    });
   }
-
 }
