7ba2fd153faccbd2b18f21f78fb1ccff4b917005
Patrick Ng <PJLN07@gmail.com>
2019-07-27 20:14:25 -0400

    ADD: routes for homepage and individual articles

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ArticlePageComponent } from './articles/article-page/article-page.component';
+import { ArticlesComponent } from './articles/articles.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ArticlesComponent, pathMatch: 'full'},
+  { path: 'articles/:name', component: ArticlePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
