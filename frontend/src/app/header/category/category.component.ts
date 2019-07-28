50e0d1f14a7f383a6607fad7f11a45749105de85
Patrick Ng <PJLN07@gmail.com>
2019-07-27 21:55:07 -0400

    EDIT: collapsible category menu

--- frontend/src/app/header/category/category.component.ts
+++ frontend/src/app/header/category/category.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.sass']
+  styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
-
+  categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
   constructor() { }
 
   ngOnInit() {
