fd50b8161049b8a462492e85275c5c9533baa9d3
Patrick Ng <PJLN07@gmail.com>
2019-07-31 22:31:09 -0400

    ADD: route for sources

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { ArticlePageComponent } from './articles/article-page/article-page.component';
-import { ArticlesComponent } from './articles/articles.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { SourcesComponent } from './header/sources/sources.component';
+import { ArticlePageComponent } from './articles/article-page/article-page.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 const routes: Routes = [
   { path: '', component: ArticlesComponent, pathMatch: 'full'},
   { path: 'articles/:name', component: ArticlePageComponent },
-  { path: 'articles', component: ArticlesComponent }
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'sources', component: SourcesComponent }
 ];
 
 @NgModule({
