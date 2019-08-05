7dc52cb4d0dcf0764411626288a04f9be2f9cd72
Patrick Ng <PJLN07@gmail.com>
2019-08-05 02:10:00 -0400

    EDIT: fetchArticleData method

--- frontend/src/app/articles/articles.service.ts
+++ frontend/src/app/articles/articles.service.ts
@@ -1,23 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { ScrapedArticle } from './../shared/scraped-article.model';
+
 @Injectable()
 export class ArticlesService {
   constructor(private http: HttpClient) {}
 
   fetchAllArticles() {
-    return this.http.get<{articles: Object[], status: String, totalresults: Number}>('http://localhost:5000');
+    return this.http.get<{articles: object[], status: string, totalresults: number}>('http://localhost:5000');
   }
 
-  fetchArticlesByType(type: String) {
-    return this.http.get<{articles: Object[], status: String, totalresults: Number}>(`http://localhost:5000/categories/${type}`);
+  fetchArticlesByType(type: string) {
+    return this.http.get<{articles: object[], status: string, totalresults: number}>(`http://localhost:5000/categories/${type}`);
   }
 
-  fetchArticlesBySource(source: String) {
-    return this.http.get<{articles: Object[]}>(`http://localhost:5000/sources/${source}`);
+  fetchArticlesBySource(source: string) {
+    return this.http.get<{articles: object[]}>(`http://localhost:5000/sources/${source}`);
   }
 
-  fetchArticleData(url: String) {
-    return this.http.post<{title: String, content: String[]}>('http://localhost:5000/articles/scrape', { url });
+  fetchArticleData(url: string, name: string) {
+    return this.http.post<{data: ScrapedArticle}>('http://localhost:5000/articles/scrape', { url, name });
   }
 }
